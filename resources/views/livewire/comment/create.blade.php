79bfae05eadbb0e0305b0be8ce9c170dd35ceb98
poymanov <n.poymanov@gmail.com>
2021-01-11 20:55:43 +0300

    Исправлена ошибка nginx на ограничение размера загружаемого файла

--- resources/views/livewire/comment/create.blade.php
+++ resources/views/livewire/comment/create.blade.php
@@ -18,7 +18,7 @@
                         <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                     </svg>
                     <span class="mt-2 text-base leading-normal">{{ __('comment.image.choose') }}</span>
-                    <input id="image" type='file' class="hidden" wire:change="$emit('fileChoosen')" />
+                    <input id="image" type='file' accept="image/*" class="hidden" wire:change="$emit('fileChoosen')" />
                 </label>
                 @if($image)
                     <img src={{$image}} width="300" />
